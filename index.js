738c88dcdc01b0f153158de4aa32c1493c9f64ed
faisalkhalil799 <35756672+faisalkhalil799@users.noreply.github.com>
2021-08-02 12:58:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var bt=document.getElementById('button');
+var div=document.getElementById('about');
+var p=document.getElementById('para');
+var skill=document.getElementById('skills');
+var edu=document.getElementById('educational details');
+var port=document.getElementById('portfolio');
+function toggle(){
+	div.classList.add("about-change");
+	p.classList.add("para-class")
+	skill.classList.add('skill-display1')
+	edu.classList.add('skill-display1')
+	port.classList.add('skill-display1')
+}
